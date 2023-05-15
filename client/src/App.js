afc037e044ee00b7c7403335a5f6e03115012852
LOrdi15 <ordieres1508@hotmail.com>
2023-05-14 20:20:03 -0600

    Se carga proyecto

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import LoginPage from './LoginPage';
+
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/adduser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+      if (response.ok) {
+        console.log('User added successfully');
+      } else {
+        console.log('An error occurred');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <input type="submit" value="Login" />
+    </form>
+  );
+}
+
+export default LoginPage;
---
+++ server/database.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  password: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "login_system"
+
+
+});
+
+
+module.exports = pool;
+
+
